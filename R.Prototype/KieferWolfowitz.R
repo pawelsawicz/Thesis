e48a75dee3f2f052719637a2d21e136e26ade7d5
pawelsawicz <pawel@sawicz.eu>
2014-10-30 01:12:06 +0100

    K-W algorith - working version, checking left and right bound of sampled function

--- R.Prototype/KieferWolfowitz.R
+++ R.Prototype/KieferWolfowitz.R
@@ -3,20 +3,22 @@ FuctionToSolve <- function(x)
   y <- ((x^2 + 0.1*x)*sin(x));
 }
 
-TestFunction <- function(x){
-  y <- x^2
-}
-
-KieferWolfowitz <- function(FUN, randomPoint){
-  x <- seq(3,6,0.1);
+KieferWolfowitz <- function(FUN, randomPoint, domain1, domain2){
+  x <- seq(domain1,domain2,0.1);
   y <- FUN(x);
-  numberOfIterations <- 10;
+  numberOfIterations <- 30;
   oldPoint <- randomPoint;
   allPoints <- c();
   for(n in 1:numberOfIterations){
     An <- (1 / (n + 1));
     Cn <- (2/(n^(1/6)));
     newPoint <- oldPoint - An * ((FUN(oldPoint + Cn)-FUN(oldPoint - Cn))/(Cn));
+    if(newPoint < domain1){
+      newPoint <- domain1
+    }
+    if(newPoint > domain2){
+      newPoint <- domain2;
+    }
     allPoints<- c(allPoints, newPoint);
     oldPoint <- newPoint
   }
@@ -24,14 +26,3 @@ KieferWolfowitz <- function(FUN, randomPoint){
   randomPoint
   allPoints  
 }
-
-StandardMin <- function(FUN){
-  #x <- seq(4,6,0.1);
-  x <- seq(-4,4,0.1);
-  y <- FUN(x);
-  plot(x,y, 'l')
-  min(y) 
-}
-
-#KieferWolfowitz(FuctionToSolve, runif(1,3,6));
-
