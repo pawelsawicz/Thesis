c91753a1ef9d2c4da448234ed79393be6a362e1c
pawelsawicz <pawel@sawicz.eu>
2014-10-30 01:17:35 +0100

    create K-W test version with visible all data

--- R.Prototype/KieferWolfowitz.R
+++ R.Prototype/KieferWolfowitz.R
@@ -26,3 +26,28 @@ KieferWolfowitz <- function(FUN, randomPoint, domain1, domain2){
   randomPoint
   allPoints  
 }
+
+
+KieferWolfowitz_Test_Version <- function(FUN, randomPoint, domain1, domain2){
+  x <- seq(domain1,domain2,0.1);
+  y <- FUN(x);
+  numberOfIterations <- 30;
+  oldPoint <- randomPoint;
+  allPoints <- c();
+  for(n in 1:numberOfIterations){
+    An <- (1 / (n + 1));
+    Cn <- (2/(n^(1/6)));
+    newPoint <- oldPoint - An * ((FUN(oldPoint + Cn)-FUN(oldPoint - Cn))/(Cn));
+    if(newPoint < domain1){
+      newPoint <- domain1
+    }
+    if(newPoint > domain2){
+      newPoint <- domain2;
+    }
+    allPoints<- c(allPoints, newPoint);
+    oldPoint <- newPoint
+  }
+  plot(x,y, 'l')
+  randomPoint
+  allPoints  
+}
