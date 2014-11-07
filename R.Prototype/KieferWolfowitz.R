269c6cc748a34356b4fc43d0d5e5dc6bf08f6869
pawelsawicz <pawel@sawicz.eu>
2014-11-07 22:22:00 +0100

    clean up algorithm, and add one test version of RS, with marked random points after all iterations

--- R.Prototype/KieferWolfowitz.R
+++ R.Prototype/KieferWolfowitz.R
@@ -1,14 +1,8 @@
-FuctionToSolve <- function(x)
-{
-  y <- ((x^2 + 0.1*x)*sin(x));
-}
-
 KieferWolfowitz <- function(FUN, randomPoint, domain1, domain2){
   x <- seq(domain1,domain2,0.1);
   y <- FUN(x);
   numberOfIterations <- 30;
-  oldPoint <- randomPoint;
-  allPoints <- c();
+  oldPoint <- randomPoint;  
   for(n in 1:numberOfIterations){
     An <- (1 / (n + 1));
     Cn <- (2/(n^(1/6)));
@@ -18,11 +12,9 @@ KieferWolfowitz <- function(FUN, randomPoint, domain1, domain2){
     }
     if(newPoint > domain2){
       newPoint <- domain2;
-    }
-    allPoints<- c(allPoints, newPoint);
+    }   
     oldPoint <- newPoint
-  }
-  plot(x,y, 'l')
+  }  
   oldPoint
 }
 
@@ -33,6 +25,7 @@ KieferWolfowitz_Test_Version <- function(FUN, randomPoint, domain1, domain2){
   numberOfIterations <- 30;
   oldPoint <- randomPoint;
   allPoints <- c();
+  plot(x,y, 'l')
   for(n in 1:numberOfIterations){
     An <- (1 / (n + 1));
     Cn <- (2/(n^(1/6)));
@@ -45,8 +38,9 @@ KieferWolfowitz_Test_Version <- function(FUN, randomPoint, domain1, domain2){
     }
     allPoints<- c(allPoints, newPoint);
     oldPoint <- newPoint
-  }
-  plot(x,y, 'l')
+    points(newPoint, FUN(newPoint));
+  }  
   randomPoint
   allPoints  
+  oldPoint
 }
--- R.Prototype/RandomSearch.R
+++ R.Prototype/RandomSearch.R
@@ -7,7 +7,8 @@ FuncMin <- function(FUN, RandomPoint, R)
 {
   domain1 <- RandomPoint - R;
   domain2 <- RandomPoint + R;  
-  KieferWolfowitz(FUN, RandomPoint, domain1, domain2)  
+  FUN(KieferWolfowitz(FUN, RandomPoint, domain1, domain2))  
+  #FUN(KieferWolfowitz_Test_Version(FUN, RandomPoint, domain1, domain2))
 }
 
 RandomSearch <- function(FUN, domain1, domain2)
@@ -29,5 +30,24 @@ RandomSearch <- function(FUN, domain1, domain2)
   bestResult
 }
 
-RandomSearch(FuctionToSolve, 0,15)
-
+RandomSearch_Test_Version <- function(FUN, domain1, domain2)
+{
+  numberOfIterations <- 40;
+  result <- c();
+  bestResult <- 0;  
+  allRandomPoints <- c();
+  x <- seq(domain1,domain2,0.1);
+  y <- FUN(x);  
+  for(i in 1:numberOfIterations)
+  {
+    randomPoint <- runif(1,domain1,domain2);
+    allRandomPoints <- c(allRandomPoints, randomPoint);
+    result <- FuncMin(FUN, randomPoint,0.5);
+    if(result <= bestResult){
+      bestResult <- result
+    }
+  }
+  plot(x,y, type='l')  
+  points(allRandomPoints, FUN(allRandomPoints))
+  bestResult
+}
