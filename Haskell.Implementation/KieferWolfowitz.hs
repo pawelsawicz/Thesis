879b0094cf5b36c6ea488bc6c01e55bbb8b47b5f
pawelsawicz <pawel@sawicz.eu>
2014-11-19 02:28:36 +0100

    add kiefer-wolfowitz implementation in haskell

--- Haskell.Implementation/KieferWolfowitz.hs
+++ Haskell.Implementation/KieferWolfowitz.hs
@@ -10,3 +10,18 @@ getAnParameter iteration = (1.0 / (iteration + 1.0))
 
 getCnParameter :: Double -> Double
 getCnParameter iteration = (2.0/(iteration**(1/6)))
+
+kieferWolfowitz :: [Double] -> Double -> [Double] --change [Double] into [Int]
+kieferWolfowitz [] _ = []
+kieferWolfowitz (x:xz) previousPoint = [calculatedPoint] ++ kieferWolfowitz xz calculatedPoint
+	where calculatedPoint = calculatePoint x previousPoint
+
+calculatePoint :: Double -> Double -> Double
+calculatePoint i oldValue
+	| newPoint > 5.0 = 5.0
+	| newPoint < (-5.0) = -5.0
+	| otherwise = newPoint
+	where
+		cn = getCnParameter(i)
+		an = getAnParameter(i)
+		newPoint = oldValue - an * ((testFunction(oldValue + cn)-testFunction(oldValue - cn))/cn)
