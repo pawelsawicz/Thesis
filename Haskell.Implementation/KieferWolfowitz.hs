4a0d1f282ab0245e904a4b45e6dfc927cbbbb8ed
pawelsawicz <pawel@sawicz.eu>
2014-11-20 02:55:37 +0100

    add domain for K-W step

--- Haskell.Implementation/KieferWolfowitz.hs
+++ Haskell.Implementation/KieferWolfowitz.hs
@@ -11,15 +11,15 @@ getAnParameter iteration = (1.0 / (iteration + 1.0))
 getCnParameter :: Double -> Double
 getCnParameter iteration = (2.0/(iteration**(1/6)))
 
-kieferWolfowitz :: [Double] -> Double -> [Double] --change [Double] into [Int]
-kieferWolfowitz [] _ = []
-kieferWolfowitz (x:xz) previousPoint = [calculatedPoint] ++ kieferWolfowitz xz calculatedPoint
-	where calculatedPoint = calculatePoint x previousPoint
+kieferWolfowitz :: [Double] -> Double -> (Double, Double)-> [Double] --change [Double] into [Int]
+kieferWolfowitz [] _ (_,_)= []
+kieferWolfowitz (x:xz) previousPoint (domain1, domain2) = [calculatedPoint] ++ kieferWolfowitz xz calculatedPoint (domain1, domain2)
+	where calculatedPoint = calculatePoint x previousPoint (domain1, domain2)
 
-calculatePoint :: Double -> Double -> Double
-calculatePoint i oldValue
-	| newPoint > 5.0 = 5.0
-	| newPoint < (-5.0) = -5.0
+calculatePoint :: Double -> Double -> (Double, Double)-> Double
+calculatePoint i oldValue (domain1, domain2)
+	| newPoint > domain1 = domain1
+	| newPoint < domain2 = domain2
 	| otherwise = newPoint
 	where
 		cn = getCnParameter(i)
