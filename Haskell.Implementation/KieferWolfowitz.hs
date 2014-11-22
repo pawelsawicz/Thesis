7ed8e307f128ebf1cc3a00a152d4d641b94963d7
pawelsawicz <pawel@sawicz.eu>
2014-11-22 18:58:07 +0100

    refactor recursive into foldl

--- Haskell.Implementation/KieferWolfowitz.hs
+++ Haskell.Implementation/KieferWolfowitz.hs
@@ -12,16 +12,14 @@ getCnParameter :: Double -> Double
 getCnParameter i = 2.0 /(i ** (1/6))
 
 calculatePoint :: Double -> Double -> (Double, Double)-> Double
-calculatePoint i oldValue (domain1, domain2)
-	| newPoint < domain1 = domain1
-	| newPoint > domain2 = domain2
+calculatePoint i oldValue (d1, d2)
+	| newPoint < d1 = d1
+	| newPoint > d2 = d2
 	| otherwise = newPoint
 	where
 		cn = getCnParameter i
 		an = getAnParameter i
 		newPoint = oldValue - an * ((testFunction(oldValue + cn)-testFunction(oldValue - cn))/cn)
 
-kieferWolfowitz :: [Double] -> Double -> (Double, Double)-> [Double]
-kieferWolfowitz [] _ (_,_)= []
-kieferWolfowitz (x:xz) previousPoint (domain1, domain2) = calculatedPoint : kieferWolfowitz xz calculatedPoint (domain1, domain2)
-	where calculatedPoint = calculatePoint x previousPoint (domain1, domain2)
+kieferWolfowitz :: [Double] -> Double -> (Double, Double) -> Double
+kieferWolfowitz xs startPoint (d1, d2) = foldl (\acc x -> calculatePoint x acc (d1,d2)) startPoint xs
